7f5067056643f03b707fc5936375dba426fb960e
Yan Ma <mayanworkmail@gmail.com>
2015-03-12 15:17:33 -0400

    Binary Tree Level Order Traversal #102 --- Accepted

--- LeetCode/src/edu/pitt/tree/BinaryTreeLevelOrderTraversal_102.java
+++ LeetCode/src/edu/pitt/tree/BinaryTreeLevelOrderTraversal_102.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Binary Tree Level Order Traversal #102
+ * Binary Tree Level Order Traversal #102 --- Accepted 229ms
  * Given a binary tree, return the level order traversal of its nodes' values. 
  * (ie, from left to right, level by level).
  * For example:
@@ -24,65 +24,78 @@ import java.util.List;
  *   [9,20],
  *   [15,7]
  * ]
- * @author yanma
- * @version 2015-03-11
+ * @author Yan Ma
  * @since 2015-03-11
- *
+ * @version 2015-03-12
  */
 public class BinaryTreeLevelOrderTraversal_102 {
     
-	public List<List<Integer>> levelOrder(TreeNode root) {
-        if (root == null) {
-        	return null;
-        }
+	public static List<List<Integer>> levelOrder(TreeNode root) {
+		List<List<Integer>> wholeList = new ArrayList<List<Integer>>();
 		
-        List<List<Integer>> wholeList = new ArrayList<List<Integer>>();
-        List<Integer> innerList = new ArrayList<Integer>();
-        List<TreeNode> nodeList = new ArrayList<TreeNode>();
-        
-        if (root != null && root.left == null && root.right == null) {
-        	innerList.add(Integer.valueOf(root.val));
-        	wholeList.add(innerList);
+        if (root == null) {
         	return wholeList;
         }
         
+        List<Integer> innerList = new ArrayList<Integer>();
+        List<TreeNode> preNodeList = new ArrayList<TreeNode>();
+        List<TreeNode> currNodeList = new ArrayList<TreeNode>();
+        
+        preNodeList.add(root);
         innerList.add(Integer.valueOf(root.val));
-    	wholeList.add(innerList);
-    	nodeList.add(root);
-    	
-    	int full = 1;
-    	int start = 0;
-    	
-    	while (full > 0) {
-    		innerList.clear();
-    		int empty = 0;
-	    	for (int i = 0; i < full; i++) {
-	    		TreeNode tempNode = nodeList.get(start + i);
-//	    		if (tempNode != null) {
-	    			if (tempNode.left == null) {
-	    				empty++;
-	    			} else {
-	    				nodeList.add(tempNode.left);
-	    			}
-	    			
-	    			
-	    			if (tempNode.right == null) {
-	    				empty++;
-	    			} else {
-	    				nodeList.add(tempNode.right);
-	    			}
-//	    		}
+        wholeList.add(innerList);
+        while (preNodeList.size() > 0) {
+        	currNodeList = new ArrayList<TreeNode>();
+        	innerList = new ArrayList<Integer>();
+        	for (TreeNode node: preNodeList) {
+	    		if (node.left != null) {
+	    			currNodeList.add(node.left);
+	    			innerList.add(Integer.valueOf(node.left.val));
+	    		}
+	    		
+	    		if (node.right != null) {
+	    			currNodeList.add(node.right);
+	    			innerList.add(Integer.valueOf(node.right.val));
+	    		}	    		
 	    	}
-    	}
+        	
+        	if (innerList.size() > 0) {
+        		wholeList.add(innerList);
+        	}
+        	preNodeList = currNodeList;
+        }
     	
-    	
+        return wholeList;
     }
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		TreeNode a1 = new TreeNode(3);
+		TreeNode a2 = new TreeNode(9);
+		TreeNode a3 = new TreeNode(20);
+		TreeNode a4 = new TreeNode(15);
+		TreeNode a5 = new TreeNode(7);
+		
+		a1.left = a2;
+		a1.right = a3;
+		a3.left = a4;
+		a3.right = a5;
+		
+		List<List<Integer>> result = levelOrder(a1);
+		System.out.println("[");
+		for (List<Integer> innerL: result) {
+			System.out.print("[");
+			
+			for (Integer value: innerL) {
+				System.out.print(value.intValue()+ ", ");
+			}
+			
+			System.out.println("]");
+		}
+		System.out.println("]");
 	}
 
 }
