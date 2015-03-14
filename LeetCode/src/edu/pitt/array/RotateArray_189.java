bee00b5a43946f5c2072da206c39cccaf8604f90
Yan Ma <mayanworkmail@gmail.com>
2015-03-14 00:12:10 -0400

    Rotate Array #189 unfinished

---
+++ LeetCode/src/edu/pitt/array/RotateArray_189.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package edu.pitt.array;
+
+/**
+ * Rotate Array #189
+ * Rotate an array of n elements to the right by k steps.
+ * For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+ * Note:
+ * Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem. 
+ * Hint:
+ * Could you do it in-place with O(1) extra space? 
+ * @author yanma
+ * @since 2015-03-13
+ * @version 2015-03-14
+ */
+public class RotateArray_189 {
+	
+    public void rotate(int[] nums, int k) {
+        
+    }
+    
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
