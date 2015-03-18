182ff1c4bed2947f39f206809523bc116a3f3ca8
Yan Ma <mayanworkmail@gmail.com>
2015-03-17 22:22:36 -0400

    Excel Sheet Column Title #168 --- Accepted

--- LeetCode/src/edu/pitt/math/ExcelSheetColumnTitle_168.java
+++ LeetCode/src/edu/pitt/math/ExcelSheetColumnTitle_168.java
@@ -4,7 +4,7 @@
 package edu.pitt.math;
 
 /**
- * Excel Sheet Column Title #168
+ * Excel Sheet Column Title #168 --- Accepted 177ms
  * Given a positive integer, return its corresponding column title as appear in an Excel sheet.
  * For example:
  *    1 -> A
@@ -14,13 +14,17 @@ package edu.pitt.math;
  *    26 -> Z
  *    27 -> AA
  *    28 -> AB 
+ *    52 -> AZ
+ *    53 -> BA
+ *    78 -> BZ
+ *    79 -> CA
  * @author yanma
  * @since 2015-03-17
  * @version 2015-03-17
  *
  */
 public class ExcelSheetColumnTitle_168 {
-    public String convertToTitle(int n) {
+    public static String convertToTitle(int n) {
         /**
          * Thoughts:
          * 1. n < 1, return "";
@@ -29,7 +33,25 @@ public class ExcelSheetColumnTitle_168 {
          * 4. n mod 26 is 0, last letter is Z, n/26 - 1 again and again to get front letters
          */
     	
+    	if (n < 1) {
+    		return  "";
+    	}
     	
+    	String[] alphabet = {"","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q",
+    						  "R","S","T","U","V","W","X","Y","Z"};
+    	    	
+    	String lastLetter = "";
+    	String frontLetter = "";
+    	
+    	if (n % 26 == 0) {
+    		lastLetter = "Z";
+    		frontLetter += convertToTitle(n/26 - 1);
+    	} else {
+    		lastLetter = alphabet[n % 26];
+    		frontLetter += convertToTitle(n/26);
+    	}
+    	
+    	return frontLetter + lastLetter;
     	
     }
 	/**
@@ -37,7 +59,15 @@ public class ExcelSheetColumnTitle_168 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("11: " + convertToTitle(11));
+		System.out.println("26: " + convertToTitle(26));
+		System.out.println("27: " + convertToTitle(27));
+		System.out.println("28: " + convertToTitle(28));
+		System.out.println("52: " + convertToTitle(52));
+		System.out.println("53: " + convertToTitle(53));
+		System.out.println("78: " + convertToTitle(78));
+		System.out.println("79: " + convertToTitle(79));
+		System.out.println("7924: " + convertToTitle(7924));
 	}
 
 }
