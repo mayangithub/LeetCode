83a479f72713b72f6d7aea41ad74c43aad0bd5c1
Yan Ma <mayanworkmail@gmail.com>
2015-03-19 19:18:07 -0400

    Climbing Stairs #70 unfinished.

---
+++ LeetCode/src/edu/pitt/dynamicprogramming/ClimbingStairs_70.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package edu.pitt.dynamicprogramming;
+
+/**
+ * Climbing Stairs #70 --- Unfinished
+ * You are climbing a stair case. It takes n steps to reach to the top.
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top? 
+ * 
+ * @author yanma
+ * @since 2015-03-19
+ * @version 2015-03-19
+ *
+ */
+public class ClimbingStairs_70 {
+    public int climbStairs(int n) {
+    	/**
+    	 * 1. find an array of number of steps: n/2 + n%2 the minimum of steps, n is the maximum of steps, 
+    	 *    1 to n/2 is the number of 2 steps in sequence
+    	 * 2. find the arrangement of steps, total position
+    	 */
+    	
+        
+    }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
