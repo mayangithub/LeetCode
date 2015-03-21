e91cf87a7d73639bbe3ea2a2b300bb2a78573ade
Yan Ma <mayanworkmail@gmail.com>
2015-03-21 17:22:35 -0400

    Two Sum #1 --- Accepted

--- LeetCode/src/edu/pitt/array/TwoSum_1.java
+++ LeetCode/src/edu/pitt/array/TwoSum_1.java
@@ -4,7 +4,7 @@
 package edu.pitt.array;
 
 /**
- * Two Sum #1
+ * Two Sum #1 --- Accepted 247ms
  * Given an array of integers, find two numbers such that they add up to a specific target number.
  * The function twoSum should return indices of the two numbers such that they add up to the target, 
  * where index1 must be less than index2. 
@@ -19,8 +19,20 @@ package edu.pitt.array;
  *
  */
 public class TwoSum_1 {
-    public int[] twoSum(int[] numbers, int target) {
-        
+    public static int[] twoSum(int[] numbers, int target) {
+    	int[] result = new int[2];
+        java.util.Hashtable<Integer, Integer> hashtable = new java.util.Hashtable<Integer, Integer>();
+    	
+        for (int i = 0; i < numbers.length; i++) {
+        	if (hashtable.containsKey(numbers[i])) {
+        		result[0] = hashtable.get(numbers[i]) + 1;
+        		result[1] = i + 1;
+        		break;
+        	} else {
+        		hashtable.put(target - numbers[i], i);
+        	}
+        }
+        return result;
     }
 
 	/**
@@ -28,6 +40,10 @@ public class TwoSum_1 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] numbers = {2,7,11,15};
+		int[] result = twoSum(numbers, 22);
+		System.out.println(result[0]);
+		System.out.println(result[1]);		
 
 	}
 
