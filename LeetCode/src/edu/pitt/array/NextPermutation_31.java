83951947b4e7c570b71052b7f6aa27c6934830ff
Yan Ma <mayanworkmail@gmail.com>
2015-03-28 14:25:40 -0400

    Next Permutation #31 --- Accepted

--- LeetCode/src/edu/pitt/array/NextPermutation_31.java
+++ LeetCode/src/edu/pitt/array/NextPermutation_31.java
@@ -4,7 +4,7 @@
 package edu.pitt.array;
 
 /**
- * Next Permutation #31
+ * Next Permutation #31 --- Accepted 240 ms
  * Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
  * If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
  * The replacement must be in-place, do not allocate extra memory.
@@ -18,8 +18,38 @@ package edu.pitt.array;
  */
 public class NextPermutation_31 {
     public void nextPermutation(int[] num) {
+    	if (num == null || num.length <= 1) {
+    		return;
+    	}
+    	
+        int temp = 0;
+        for (int j = num.length - 1; j > 0; j--) {
+        	if (num[j] > num[j - 1]) {
+        		//find the min digit which is bigger than num[j - 1] but the min of rest of array from j to length-1
+        		int min = j;
+        		for (int i = j; i < num.length; i++) {
+        			if (num[i] > num[j - 1] && num[i] < num[min]) {
+        				min = i;
+        			}
+        		}
+        		temp = num[j - 1];
+        		num[j - 1] = num[min];
+        		num[min] = temp;
+        		
+        		//sort the rest of the array, from j to last digit
+        		java.util.Arrays.sort(num, j, num.length);
+        		
+        		break;
+        	}
+        	
+        	if (j == 1) {
+        		java.util.Arrays.sort(num);
+        	}
+        }
         
     }
+    
+     
 	/**
 	 * @param args
 	 */
