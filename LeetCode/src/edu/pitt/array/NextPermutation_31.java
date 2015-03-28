968e41958df7bd77deeca7e2d221fb2697ca39b9
Yan Ma <mayanworkmail@gmail.com>
2015-03-27 22:52:31 -0400

    Next Permutation #31 unfinished

---
+++ LeetCode/src/edu/pitt/array/NextPermutation_31.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package edu.pitt.array;
+
+/**
+ * Next Permutation #31
+ * Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+ * If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+ * The replacement must be in-place, do not allocate extra memory.
+ * Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+ * 1,2,3 → 1,3,2
+ * 3,2,1 → 1,2,3
+ * 1,1,5 → 1,5,1
+ * @author yanma
+ * @since 2015-03-27
+ * @version 2015-03-27
+ */
+public class NextPermutation_31 {
+    public void nextPermutation(int[] num) {
+        
+    }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
