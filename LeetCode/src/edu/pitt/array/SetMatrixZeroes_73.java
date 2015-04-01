f8c681fe66dc68cac724d49e0f4c3d42dad97ce8
Yan Ma <mayanworkmail@gmail.com>
2015-03-31 20:59:09 -0400

    Set Matrix Zeroes #73 --- Accepted

--- LeetCode/src/edu/pitt/array/SetMatrixZeroes_73.java
+++ LeetCode/src/edu/pitt/array/SetMatrixZeroes_73.java
@@ -54,6 +54,75 @@ public class SetMatrixZeroes_73 {
         
     }
     
+    
+    /**
+     * clam king solution --- 267ms
+     * @param matrix
+     */
+    public void setZeroes1(int[][] matrix) {
+        
+
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return;
+        }
+        
+        boolean row = false;
+        boolean column = false;
+        int width = matrix[0].length;
+        int height = matrix.length;
+        
+        for (int i = 0; i < width; i++) {
+            if (matrix[0][i] == 0) {
+                row = true;
+                break;
+            }
+        }
+        
+        for (int i = 0; i < height; i++) {
+            if (matrix[i][0] == 0) {
+                column = true;
+                break;
+            }
+        }
+        
+        for (int i = 1; i < width; i++) {
+            for (int j = 1; j < height; j++) {
+                if (matrix[j][i] == 0) {
+                    matrix[0][i] = 0;
+                    matrix[j][0] = 0;
+                }
+            }
+        }
+        
+        for (int i = 1; i < width; i++) {
+            if (matrix[0][i] == 0) {
+                for (int j = 0; j < height; j++) {
+                    matrix[j][i] = 0;
+                }
+            }
+        }
+        
+        for (int j = 1; j < height; j++) {
+            if (matrix[j][0] == 0) {
+                for (int i = 0; i < width; i++) {
+                    matrix[j][i] = 0;
+                }
+            }
+        }
+        
+        if (row) {
+            for (int i = 0; i < width; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        
+        if (column) {
+            for (int i = 0; i < height; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+    
 	/**
 	 * @param args
 	 */
