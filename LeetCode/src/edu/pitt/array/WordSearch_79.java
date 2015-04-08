c5ea37975e28be23c19789dd51c015bed133e61d
Yan Ma <mayanworkmail@gmail.com>
2015-04-08 15:04:19 -0400

    Word Search #79 --- Accepted

--- LeetCode/src/edu/pitt/array/WordSearch_79.java
+++ LeetCode/src/edu/pitt/array/WordSearch_79.java
@@ -4,7 +4,7 @@
 package edu.pitt.array;
 
 /**
- * Word Search #79 unfinished
+ * Word Search #79 --- Accepted 381ms
  *  Given a 2D board and a word, find if the word exists in the grid.
  *  The word can be constructed from letters of sequentially adjacent cell, 
  *  where "adjacent" cells are those horizontally or vertically neighboring. 
@@ -52,42 +52,43 @@ public class WordSearch_79 {
 		while (index < word.length()) {
 			if (index == 0) {
 				outerloop:
-				for (; row < board.length; row++) {
-	    			col = 0;
-		    		for (; col < board[0].length; col++) {
-		    			if (board[row][col] == word.charAt(index)) {
-		    				if (trueboard[row][col] == false) {
-		    					trueboard[row][col] = true;
-		    					index++;
-		    					System.out.println("row: " + row + " col: " + col);
-		    					break outerloop;
-		    				}
-		    			} 
-		    		}
-		    	}
-	    		
-	    		if (row == board.length && col == board[0].length) {
-	    			System.out.println("row: " + row + " col: " + col);
-	    			return false;
-	    		}
+					for (; row < board.length; row++) {
+		    			col = 0;
+			    		for (; col < board[0].length; col++) {
+			    			if (board[row][col] == word.charAt(index)) {
+			    				if (trueboard[row][col] == false) {
+			    					trueboard[row][col] = true;
+			    					index++;
+			    					System.out.println("row: " + row + " col: " + col);
+			    					break outerloop;
+			    				}
+			    			} 
+			    		}
+			    	}
+		    		
+				if (row == board.length) {
+					System.out.println("row: " + row + " col: " + col);
+					return false;
+				}//not found the first letter
 	    		
 			} else {
-				if (existsub(board, word, index, row, col, trueboard) ) {
-					index++;
+				boolean[][] newtrueboard = new boolean[board.length][board[0].length];
+				newtrueboard[row][col] = true;
+				if (existsub(board, word, index, row, col, newtrueboard) ) {
+					return true;
 				} else {
-					index--;
+					index = 0;
+					trueboard[row][col] = true;
 				}
 			}
 		}
 		
 		return true;
-        
+		        
     }
     
-    public static boolean existsub(char[][] board, String word, int startindex, int startrow, int startcol, boolean[][] trueboard) {
-    	int index = startindex;
-    	int row = startrow;
-    	int col = startcol;
+    public static boolean existsub(char[][] board, String word, int index, int row, int col, boolean[][] trueboard) {
+    	trueboard[row][col] = true;
     	
     	if (index < word.length()) {
     		/**
@@ -95,9 +96,14 @@ public class WordSearch_79 {
     		 */
     		if (row > 0) {
     			if (board[row - 1][col] == word.charAt(index) && trueboard[row - 1][col] == false ) {
-    				row = row - 1; 
-    				trueboard[row][col] = true;
     				
+    				if (existsub(board, word, index + 1, row - 1, col, trueboard)) {
+        				System.out.println("index: " + index + " row: " + (row - 1) + " col: " + col);
+    					return true;
+    				} else {
+    					System.out.println("above doesn't work.");
+    					trueboard[row - 1][col] = false;
+    				}
     			}
     		}
     		
@@ -106,13 +112,14 @@ public class WordSearch_79 {
     		 */
     		if (row < board.length - 1) {
     			if (board[row + 1][col] == word.charAt(index) && trueboard[row + 1][col] == false) {
-    				row = row + 1;
-    				trueboard[row][col] = true;
-    				if (existsub(board, word, index + 1, row, col, trueboard)) {
+    				
+    				if (existsub(board, word, index + 1, row + 1, col, trueboard)) {
+        				System.out.println("index: " + index + " row: " + (row + 1) + " col: " + col);
     					return true;
+    				} else {
+    					System.out.println("below doesn't work.");
+    					trueboard[row + 1][col] = false;
     				}
-    				System.out.println("below: row: " + row + " col: " + col);
-    				row = row - 1;
     			}
     		}
     		
@@ -121,13 +128,15 @@ public class WordSearch_79 {
     		 */
     		if (col > 0) {
     			if (board[row][col - 1] == word.charAt(index) && trueboard[row][col - 1] == false) {
-    				col = col - 1;
-    				trueboard[row][col] = true;
-    				if (existsub(board, word, index + 1, row, col, trueboard)) {
+    				
+    				if (existsub(board, word, index + 1, row, col - 1, trueboard)) {
+        				System.out.println("index: " + index + " row: " + row + " col: " + (col - 1));
     					return true;
+    				} else {
+    					System.out.println("left doesn't work.");
+    					trueboard[row][col - 1] = false;
     				}
-    				System.out.println("left: row: " + row + " col: " + col);
-    				col = col + 1;
+    				
     			}
     		}
     		
@@ -136,24 +145,25 @@ public class WordSearch_79 {
     		 */
     		if (col < board[0].length - 1) {
     			if (board[row][col + 1] == word.charAt(index) && trueboard[row][col + 1] == false) {
-    				col = col + 1;
-    				trueboard[row][col] = true;
-    				if (existsub(board, word, index + 1, row, col, trueboard)) {
+    				
+    				if (existsub(board, word, index + 1, row, col + 1, trueboard)) {
+        				System.out.println("index: " + index + " row: " + row + " col: " + (col + 1));
     					return true;
+    				} else {
+    					System.out.println("right doesn't work.");
+    					trueboard[row][col + 1] = false;
     				}
-    				System.out.println("right: row: " + row + " col: " + col);
-    				col = col - 1;
+    				
     			}
     		}
         	
-    		System.out.println("not found: row: " + row + " col: " + col);
-    		trueboard[row][col] = true;
     	} else {
     		return true;
     	}
     	
-    	return false;
-		
+		return false;
+
+    			
     }
 	/**
 	 * @param args
@@ -180,8 +190,38 @@ public class WordSearch_79 {
 		board1[2][2] = 'E';
 		board1[2][3] = 'E';
 		
-		//System.out.println(exist(board, "cdba"));
+		
+		char[][] board2 = new char[3][3];
+		board2[0][0] = 'C';
+		board2[0][1] = 'A';
+		board2[0][2] = 'A';
+		board2[1][0] = 'A';
+		board2[1][1] = 'A';
+		board2[1][2] = 'A';
+		board2[2][0] = 'B';
+		board2[2][1] = 'C';
+		board2[2][2] = 'D';
+		
+		char[][] board3 = new char[3][4];
+		board3[0][0] = 'A';
+		board3[0][1] = 'B';
+		board3[0][2] = 'C';
+		board3[0][3] = 'E';
+		board3[1][0] = 'S';
+		board3[1][1] = 'F';
+		board3[1][2] = 'E';
+		board3[1][3] = 'S';
+		board3[2][0] = 'A';
+		board3[2][1] = 'D';
+		board3[2][2] = 'E';
+		board3[2][3] = 'E';
+		
+		
+		
+		System.out.println(exist(board, "cdba"));
 		System.out.println(exist(board1, "SEE"));
+		System.out.println(exist(board2, "AAB"));
+		System.out.println(exist(board3, "ABCESEEEFS"));
 		
 	}
 
