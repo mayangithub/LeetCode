e88de67ce2ef36ac71c3484efcd2e08bddb4312d
Yan Ma <mayanworkmail@gmail.com>
2015-05-02 23:07:30 -0400

    Binary Tree Preorder Traversal #144 start

---
+++ LeetCode/src/edu/pitt/tree/BinaryTreePreorderTraversal_144.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package edu.pitt.tree;
+
+/**
+ * Binary Tree Preorder Traversal #144
+ * Given a binary tree, return the preorder traversal of its nodes' values.
+ * For example:
+ * Given binary tree {1,#,2,3},
+ *  1
+ *   \
+ *    2
+ *   /
+ *  3
+ * return [1,2,3].
+ * Note: Recursive solution is trivial, could you do it iteratively?
+ * @author yanma
+ * @since 2015-05-02
+ * @version 2015-05-02
+ */
+public class BinaryTreePreorderTraversal_144 {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        
+    }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
