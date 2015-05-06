ff65ce91fea4cd16705774e3ebbccda603e36070
Yan Ma <mayanworkmail@gmail.com>
2015-05-05 23:58:45 -0400

    Single Number #136 start

---
+++ LeetCode/src/edu/pitt/hashtable/SingleNumber_136.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package edu.pitt.hashtable;
+
+/**
+ * Single Number #136
+ * Given an array of integers, every element appears twice except for one. Find that single one.
+ * Note:
+ * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory? 
+ * @author yanma
+ * @since 2015-05-05
+ * @version 2015-05-05
+ */
+public class SingleNumber_136 {
+    public int singleNumber(int[] nums) {
+        
+    }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
