dc733524fa900c53bae66b587723bb08a26643f6
Yan Ma <mayanworkmail@gmail.com>
2015-05-08 16:14:04 -0400

    Best Time to Buy and Sell Stock #121 --- new Accepted faster solution

--- LeetCode/src/edu/pitt/dynamicprogramming/BestTimetoBuyandSellStock_121.java
+++ LeetCode/src/edu/pitt/dynamicprogramming/BestTimetoBuyandSellStock_121.java
@@ -4,7 +4,7 @@
 package edu.pitt.dynamicprogramming;
 
 /**
- * Best Time to Buy and Sell Stock #121 --- Accepted 421ms
+ * Best Time to Buy and Sell Stock #121 --- Accepted 421ms/299ms
  * Say you have an array for which the ith element is the price of a given stock on day i.
  * If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), 
  * design an algorithm to find the maximum profit.
@@ -13,6 +13,12 @@ package edu.pitt.dynamicprogramming;
  * @version 2015-05-07
  */
 public class BestTimetoBuyandSellStock_121 {
+	
+	/**
+	 * Accepted 421ms
+	 * @param prices
+	 * @return
+	 */
     public int maxProfit(int[] prices) {
         if (prices == null || prices.length < 2) {
         	return 0;
@@ -63,6 +69,45 @@ public class BestTimetoBuyandSellStock_121 {
            
         return gap;
     }
+    
+    /**
+     * Accepted 299ms
+     * Note: 
+     * 1. cautious when new min appears, max would be invalid
+     * 2. cautious when new max appears, old max would be invalid
+     * @param prices
+     * @return
+     */
+    public int maxProfit1(int[] prices) {
+    	if (prices == null || prices.length < 2) {
+        	return 0;
+        } else if (prices.length == 2 && prices[0] > prices[1]) {
+        	return 0;
+        }
+        
+    	int minPrice = prices[0];
+    	int maxPrice = prices[0];
+        int maxProfit = 0;
+        
+        for (int index = 1; index < prices.length; index++) {
+        	if (prices[index] < minPrice) {
+        		minPrice = prices[index];
+        	}
+        	
+        	if (prices[index] >= maxPrice) { // new maxPrice, even the same with old maxPrice, calculate profit
+        		maxPrice = prices[index];
+        		maxProfit = (maxPrice - minPrice > maxProfit) ? maxPrice - minPrice: maxProfit;
+        	}
+        	
+        	if (prices[index] > minPrice && prices[index] < maxPrice && prices[index] - minPrice > maxProfit) {
+        		maxPrice = prices[index];
+        		maxProfit = maxPrice - minPrice;
+        	}
+        }
+           
+        return maxProfit;
+    }
+    
 	/**
 	 * @param args
 	 */
