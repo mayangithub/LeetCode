5006bf8a7505605133b88784130295b97d306b8f
Yan Ma <mayanworkmail@gmail.com>
2015-05-21 17:31:16 -0400

    Decode Ways #91 --- Accepted

---
+++ LeetCode/src/edu/pitt/linkedlist/ConvertSortedListtoBinarySearchTree_109.java
@@ -0,0 +1,124 @@
+/**
+ * 
+ */
+package edu.pitt.linkedlist;
+
+import edu.pitt.tree.TreeNode;
+
+/**
+ * Convert Sorted List to Binary Search Tree #109 --- Accepted 380ms
+ * Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+ * @author yanma
+ * @since 2015-05-15
+ * @version 2015-05-15
+ */
+public class ConvertSortedListtoBinarySearchTree_109 {
+	private ListNode currNode;
+	/**
+	 * Definition for singly-linked list.
+	 * public class ListNode {
+	 *     int val;
+	 *     ListNode next;
+	 *     ListNode(int x) { val = x; }
+	 * }
+	 */
+	/**
+	 * Definition for a binary tree node.
+	 * public class TreeNode {
+	 *     int val;
+	 *     TreeNode left;
+	 *     TreeNode right;
+	 *     TreeNode(int x) { val = x; }
+	 * }
+	 */
+    public TreeNode sortedListToBST(ListNode head) {
+    	//find middle list element, make it root
+    	//to left side, repeat; to right side, repeat
+        if (head == null) {
+        	return null;
+        } else if (head.next == null) {
+        	TreeNode node = new TreeNode(head.val);
+        	return node;
+        }
+        
+        int size = getSize(head);
+        currNode = head;
+        TreeNode rootNode = construct(size);
+        return rootNode;
+    }
+    
+    public int getSize(ListNode head) {
+    	int size = 0;
+    	while (head != null) {
+    		size++;
+    		head = head.next;
+    	}
+    	return size;
+    }
+    
+    /**
+     * 
+     * @param size
+     * @return
+     */
+    public TreeNode construct(int size) {
+    	if (size <= 0) {
+    		return null;
+    	}
+    	
+    	TreeNode left = construct(size / 2);
+    	// to list[0] node, as leftmost node, then, list[1] node as root, then list[2] node as right
+    	// then currNode to list[3], as root, next three nodes as right branch
+    	// then currNode to list[7] as root, next seven nodes as right branch
+    	// then currNode to list[14] as root......
+    	TreeNode root = new TreeNode(currNode.val);
+    	currNode = currNode.next;
+    	TreeNode right = construct(size - 1 - size / 2); //size left
+    	
+    	root.left = left;
+    	root.right = right;
+    	
+    	return root;
+    }
+        
+    public TreeNode sortedListToBST1(ListNode head) {
+    	if (head == null) {
+        	return null;
+        } else if (head.next == null) {
+        	TreeNode node = new TreeNode(head.val);
+        	return node;
+        }
+        
+        int size = getSize(head);
+        
+        TreeNode root = constrctTree(head, size);
+        
+        return root;
+    }
+    
+    
+    public TreeNode findNode(ListNode head, int index) {
+    	int count = 0;
+    	while (count != index) {
+			
+		}
+    	
+    	
+    }
+    
+    public TreeNode constrctTree(ListNode head, int length) {
+    	
+    }
+    
+    
+    
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- LeetCode/src/edu/pitt/tree/TreeNode.java
+++ LeetCode/src/edu/pitt/tree/TreeNode.java
@@ -10,8 +10,8 @@ package edu.pitt.tree;
  */
 public class TreeNode {
 	int val;
-	TreeNode left;
-	TreeNode right;
+	public TreeNode left;
+	public TreeNode right;
 	public TreeNode(int x) {
 		val = x; 
 	}
