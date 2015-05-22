bb22140c73b64c673501401173ab3d2829b3a3f2
Yan Ma <mayanworkmail@gmail.com>
2015-05-22 18:43:34 -0400

    Repeated DNA Sequences #187 --- Accepted

--- LeetCode/src/edu/pitt/hashtable/RepeatedDNASequences_187.java
+++ LeetCode/src/edu/pitt/hashtable/RepeatedDNASequences_187.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Repeated DNA Sequences #187
+ * Repeated DNA Sequences #187 --- Accepted 524ms/484ms
  *  All DNA is composed of a series of nucleotides abbreviated as A, C, G, and T, for example: "ACGAATTCCG". 
  *  When studying DNA, it is sometimes useful to identify repeated sequences within the DNA.
  *  Write a function to find all the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule.
@@ -17,9 +17,15 @@ import java.util.List;
  *  ["AAAAACCCCC", "CCCCCAAAAA"].
  * @author yanma
  * @since 2015-04-23
- * @version 2015-04-23
+ * @version 2015-05-22
  */
 public class RepeatedDNASequences_187 {
+	
+	/**
+	 * Accepted 524ms
+	 * @param s
+	 * @return
+	 */
     public static List<String> findRepeatedDnaSequences(String s) {
     	List<String> list = new ArrayList<String>();
         if (s == null || s.length() <= 10) {
@@ -39,19 +45,72 @@ public class RepeatedDNASequences_187 {
 	        		}
         		}
         	}
-        	
         }
-        
-    	
     	return list;
     }
+    
+    
+    /**
+     * Accepted 484ms
+     * @param s
+     * @return
+     * using bit manipulation to transform String to integer
+     * comparing integers would be faster than comparing Strings
+     */
+    public static List<String> findRepeatedDnaSequences1(String s) {
+    	List<String> list = new ArrayList<String>();
+        if (s == null || s.length() <= 10) {
+        	return list;
+        }
+        
+        java.util.Hashtable<Integer, Integer> hashtable = new java.util.Hashtable<>();
+        
+        for (int start = 0; start < s.length() - 9; start++) {
+        	int number = 0;
+        	for (int end = start; end < start + 10; end++) {
+        		switch (s.charAt(end)) {
+					case 'A':
+						number |= 0;
+						break;
+					case 'C':
+						number |= 1;
+						break;
+					case 'G':
+						number |= 2;
+						break;
+					default: // 'T'
+						number |= 3;
+						break;
+				} 
+        		number = number << 2;
+        	}
+        	
+        	if (!hashtable.containsKey(number)) {
+        		hashtable.put(number, 1);
+        	} else {
+        		switch (hashtable.get(number)) {
+				case 1:
+					list.add(s.substring(start, start + 10));
+        			hashtable.put(number, 2);
+					break;
+				default:
+					break;
+				}
+        	}
+        }
+        
+        return list;
+    }
+    
+    
+    
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
-		List<String> list = findRepeatedDnaSequences(s);
+		List<String> list = findRepeatedDnaSequences1(s);
 		for (String string : list) {
 			System.out.println(string);
 		}
