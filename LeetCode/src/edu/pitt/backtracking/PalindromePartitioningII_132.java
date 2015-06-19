e2e0f6970d0556335ec3654aa343fc99aa8e733e
Yan Ma <mayanworkmail@gmail.com>
2015-06-18 22:58:02 -0400

    Palindrome Partitioning II #132 --- Accepted

--- LeetCode/src/edu/pitt/backtracking/PalindromePartitioningII_132.java
+++ LeetCode/src/edu/pitt/backtracking/PalindromePartitioningII_132.java
@@ -31,10 +31,11 @@ public class PalindromePartitioningII_132 {
 		}
         
         for (int i = 0; i < s.length(); i++) {
+        	//odd length palindrome
 			for (int j = 0; j <= i && i + j < s.length() && s.charAt(i - j) == s.charAt(i + j); j++) {
-				cut[i + j + 1] = Math.min(cut[i + j + 1], cut[i - j] + 1);
+				cut[i + j + 1] = Math.min(cut[i + j + 1], cut[i - j] + 1); 
 			}
-			
+			//even length palindrome
 			for (int j = 1; i - j + 1 >= 0 && i + j < s.length() && s.charAt(i - j + 1) == s.charAt(i + j) ; j++) {
 				cut[i + j + 1] = Math.min(cut[i + j + 1], cut[i - j + 1] + 1);
 			}
