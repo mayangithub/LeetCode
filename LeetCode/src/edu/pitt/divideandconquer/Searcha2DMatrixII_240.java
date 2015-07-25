e34abdaea6d5453d4a9a4ca664c8d2847b99ad74
Yan Ma <mayanworkmail@gmail.com>
2015-07-24 23:38:07 -0400

    Search a 2D Matrix II #240

---
+++ LeetCode/src/edu/pitt/divideandconquer/Searcha2DMatrixII_240.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package edu.pitt.divideandconquer;
+
+/**
+ * Search a 2D Matrix II #240
+ * @author yanma
+ * @since 2015-07-24
+ * @version 2015-07-24
+ */
+public class Searcha2DMatrixII_240 {
+
+}
